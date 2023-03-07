5c2097f0951b97aee4e58f10fd2211c511aa2f05
DESKTOP-6OVVB6Q\Askyr1m <akatokikwok@outlook.com>
2023-03-07 10:26:52 +0800

    [ver15, feat]: 在多人会话的Subsystem内管理1个会话接口指针.

---
+++ Plugins/MultiplayerSessions/Source/MultiplayerSessions/Private/MultiplayerSessionsSubsystem.cpp
@@ -0,0 +1,14 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MultiplayerSessionsSubsystem.h"
+#include "OnlineSubsystem.h"
+
+UMultiplayerSessionsSubsystem::UMultiplayerSessionsSubsystem()
+{
+	/* 在构造器内拿到 在线系统的单例, 并用单例去读取会话接口.*/
+	IOnlineSubsystem* Subsystem = IOnlineSubsystem::Get();
+	if (Subsystem) {
+		SessionInterface = Subsystem->GetSessionInterface();
+	}
+}
---
+++ Plugins/MultiplayerSessions/Source/MultiplayerSessions/Public/MultiplayerSessionsSubsystem.h
@@ -0,0 +1,24 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "Interfaces/OnlineSessionInterface.h"
+#include "MultiplayerSessionsSubsystem.generated.h"
+
+/**
+ * 多人会话管理子系统
+ */
+UCLASS()
+class MULTIPLAYERSESSIONS_API UMultiplayerSessionsSubsystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+public:
+	UMultiplayerSessionsSubsystem();
+protected:
+
+private:
+	// 在线会话接口; 本质是1根OnlineSession智能指针;
+	IOnlineSessionPtr SessionInterface;
+};
